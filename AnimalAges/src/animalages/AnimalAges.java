c5f594988a04aefe7ea19028e998a92f2e2d1e11
julia <julia@MillerDell>
2026-02-05 21:02:14 -0700

    fixed array list and added avg age

--- AnimalAges/src/animalages/AnimalAges.java
+++ AnimalAges/src/animalages/AnimalAges.java
@@ -10,21 +10,34 @@ public class AnimalAges
 
 		static ArrayList<String> animals = new ArrayList<String>();
 		
-		public static void main() throws IOException{
+		public static void main(String[] args) throws Exception {
 			Scanner myFile = new Scanner(new File("animal.txt"));
 			
 			while(myFile.hasNextLine()) {
 				
 				String word = myFile.nextLine();
 				
-				animals.add(word.split(","));
+				animals.add(word);
 				
-				String species = animals[0];
+				//String species = animals[0];
 				
-				Collections.sort( );
+				Collections.sort(animals);
+				
 				
 			}
 			
+			
+			//print list
+			int sumAges = 0;
+			for(int x = 0; x < animals.size();  x++) {
+				String animal = animals.get(x);
+				int age = Integer.parseInt(animal.split(",")[1]);
+				sumAges = sumAges+age;
+				
+				System.out.println(animals.get(x));
+			}
+			double averageAge = (double)sumAges/animals.size();
+			System.out.println("Average Age: " + averageAge);
 		}
 		
 		
